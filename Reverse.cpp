f76e980a12d5464d6fe59a0d125d87fd582ddc31
NS <evilnood@gmail.com>
2021-10-28 20:34:20 +0300

    Добавьте файлы проекта.

---
+++ Reverse.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+std::string invert_s(std::string str)
+{
+	for (int i = 0; i < str.length() / 2; i++)
+	{
+		std::swap(str[i], str[str.length() - i-1]);
+	}
+	return str;
+}
+
+int main()
+{
+	std::string string;
+	std::cin >> string;
+
+	invert_s(string);
+
+	std::cout << invert_s(string);
+
+	return 0;
+}
